805480f72d19559d9ace7d7f735ecddd9658d77a
kr0st <kr0st@bioreactor.me>
2017-10-17 18:49:03 +0300

    Lua added to dependencies, issue #9 fixed.

--- dependencies/macos/get_and_build_other.sh
+++ dependencies/macos/get_and_build_other.sh
@@ -36,6 +36,7 @@ mkdir ../build
 curl -L --cacert ./cacert.crt -o ../build/date.zip https://github.com/HowardHinnant/date/archive/v2.2.zip
 curl -L --cacert ./cacert.crt -o ../build/gtest.zip https://codeload.github.com/google/googletest/zip/release-1.8.0
 curl -L --cacert ./cacert.crt -o ../build/chai.zip https://github.com/ChaiScript/ChaiScript/archive/v6.0.0.zip
+curl -R -o ../build/lua-5.3.4.tar.gz http://www.lua.org/ftp/lua-5.3.4.tar.gz
 
 7za x -y -o"../build/" ../build/date.zip
 7za x -y -o"../build/" ../build/gtest.zip
@@ -47,16 +48,37 @@ export CPPFLAGS="-I/usr/local/opt/openssl/include"
 cd ..
 cd ./build
 
+tar zxf lua-5.3.4.tar.gz
+cd ./lua-5.3.4
+make macosx test MYCFLAGS="-arch x86_64"
+
+echo 'liblua5.3.4.dylib: $(CORE_O) $(LIB_O)' >> src/makefile
+echo -e '\t$(CC) -dynamiclib -o $@ $^ $(LIBS) -arch x86_64 -compatibility_version 5.3.0 -current_version 5.3.4 -install_name @rpath/$@' >> src/makefile
+make -C src liblua5.3.4.dylib
+
+make macosx local MYCFLAGS="-arch x86_64"
+
+mkdir ../../include
+
+mkdir ../../lib
+mkdir ../../lib/x64
+
+rm -rf ../../include/lua
+rm -rf ../../lib/x64/*lua*
+
+mkdir ../../include/lua
+
+cp -rf ./install/lib/liblua.a ../../lib/x64
+cp -rf ./src/liblua5.3.4.dylib ../../lib/x64
+cp -rf ./install/include/* ../../include/lua
+
+cd ..	
+
 cd googletest-release-1.8.0
 cd googletest
 
 rm -rf ../../../include/gtest
-rm -rf ../../../lib/gtest
-
-mkdir ../../../include
-
-mkdir ../../../lib
-mkdir ../../../lib/x64
+rm -rf ../../../lib/x64/gtest
 
 cmake ./
 make
