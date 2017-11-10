bf85f5ec897e126873eef4158cb3c0166bff924b
kr0st <kr0st@bioreactor.me>
2017-11-10 14:05:27 +0200

    Default configuration implemented.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include "mainwindow.h"
+
 #include <QApplication>
+
+#include <globals.h>
+#include <settings.h>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QCoreApplication::setOrganizationName(fpgui::settings::author);
+    QCoreApplication::setOrganizationDomain(fpgui::settings::domain);
+    QCoreApplication::setApplicationName(fpgui::settings::application_name);
+
+    fpgui::settings::make_config_path();
+
+    QSettings::setDefaultFormat(QSettings::IniFormat);
+    QSettings settings;
+
+    settings.setFallbacksEnabled(false);
+
+    std::string section(fpgui::settings::db_section_name);
+    section += '/';
+
+    if (!settings.contains(QString((section + fpgui::settings::db_host_setting).c_str())))
+    {
+        settings.clear();
+        fpgui::settings::write_default_settigs(settings);
+    }
+
     MainWindow w;
     w.show();
 
