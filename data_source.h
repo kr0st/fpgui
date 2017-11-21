f3b338c14d6bdb2e97d1a9b6e2d641e36471ed6e
kr0st <kr0st@bioreactor.me>
2017-11-21 20:14:01 +0200

    Random data source implemented but not tested.

--- data_source.h
+++ data_source.h
@@ -52,31 +52,59 @@ template <typename T=std::vector<std::string>> void generate_json_strings(T& gen
 
     for (size_t i = 0; i < how_many; ++i)
     {
-        int text_size = text_size_bytes_min + qrand() % (text_size_bytes_max - text_size_bytes_min);
+        int text_size = text_size_bytes_min;
+
+        if (text_size_bytes_max - text_size_bytes_min > 0)
+            text_size += qrand() % (text_size_bytes_max - text_size_bytes_min);
+
         std::string text;
         text.resize(text_size);
+
         for (int j = 0; j < text_size; ++j)
             text[j] = random_bytes[j];
+
         std::string rnd_msg = "{\"timestamp\":\"" + random_timestamp() + "\", \"sequence\":" + std::to_string(qrand() % 666) +
                               ", \"hostname\":\"" + hosts[qrand() % hosts.size()] + "\", \"text\":\"" + text +  "\" }";
+
         generated.push_back(rnd_msg);
     }
 }
 
 }
 
+template <typename T=std::queue<std::string>>
 class Data_Source
 {
   public:
 
-      virtual void request_data(std::queue<std::string>& data) = 0;
+      virtual void request_data(T& data) = 0;
 };
 
-class Random_Data_Source: public Data_Source
+template <typename T=std::queue<std::string>>
+class Random_Data_Source: public Data_Source<T>
 {
-  public:
+    public:
 
-      void request_data(std::queue<std::string> &data) {}
+        void set_batch_size(int batch_sz){ set_batch_size(batch_sz, batch_sz); }
+        void set_batch_size(int min_sz, int max_sz) { min_sz_batch_ = min_sz; max_sz_batch_ = max_sz; }
+
+        void set_single_string_size(int string_sz) { set_single_string_size(string_sz, string_sz); }
+        void set_single_string_size(int min_sz, int max_sz){ min_sz_string_ = min_sz; max_sz_string_ = max_sz; }
+
+        void request_data(T &data)
+        {
+            int batch_sz = min_sz_batch_;
+
+            if (max_sz_batch_ - min_sz_batch_ > 0)
+                batch_sz += qrand() % (max_sz_batch_ - min_sz_batch_);
+
+            random::generate_json_strings(data, batch_sz, min_sz_string_, max_sz_string_);
+        }
+
+
+    private:
+
+        int min_sz_batch_ = 0, max_sz_batch_ = 0, min_sz_string_ = 0, max_sz_string_ = 0;
 };
 
 }}
