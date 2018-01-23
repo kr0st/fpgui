427aca06889b2715de782bba0a49e561f5b344fb
kr0st <kr0st@bioreactor.me>
2018-01-23 17:32:39 +0200

    MongoDB data source draft implementation functional.
    Not complete though and does not take the config into consideration,
    just connects locally & without password.

--- mongo_data_source.cpp
+++ mongo_data_source.cpp
@@ -2,7 +2,9 @@
 #include <mongocxx/stdx.hpp>
 #include <mongocxx/uri.hpp>
 #include <mongocxx/instance.hpp>
+#include <bsoncxx/types.hpp>
 #include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
 
 #include <mongo_data_source.h>
 
@@ -24,12 +26,26 @@ template <> void Mongo_Data_Source<std::queue<std::string>>::connect(const setti
 
 template <> void Mongo_Data_Source<std::queue<std::string>>::request_data(std::queue<std::string>& data)
 {
+    static std::string last_id;
+
     mongocxx::database fplog = (*client_)["fplog"];
     mongocxx::collection logs = fplog["logs"];
 
-    mongocxx::cursor cur = logs.find(bsoncxx::builder::stream::document{} << bsoncxx::builder::stream::finalize);
-    for (const bsoncxx::document::view& doc: cur)
+    std::unique_ptr<mongocxx::cursor> cur;
+
+    if (last_id.empty())
+        cur.reset(new mongocxx::cursor(logs.find(bsoncxx::builder::stream::document{} << bsoncxx::builder::stream::finalize)));
+    else
+        cur.reset(new mongocxx::cursor(
+                      logs.find(bsoncxx::builder::stream::document{} << "_id" << bsoncxx::builder::stream::open_document <<
+                                "$gt" << bsoncxx::oid(last_id) << bsoncxx::builder::stream::close_document <<
+                                bsoncxx::builder::stream::finalize)));
+
+    for (const bsoncxx::document::view& doc: *cur)
     {
+        const bsoncxx::document::element& id = doc["_id"];
+        last_id = id.get_oid().value.to_string();
+        data.push(bsoncxx::to_json(doc));
     }
 }
 
